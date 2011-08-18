ba32f87b04caedcc731d7357f60cc74af0e2a6d9
Tomas <tomas.hluchan@gmail.com>
2011-08-18 13:14:21 +0200

    nastavovani systemovych promennych

--- admin/urls.py
+++ admin/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('admin.views',
     (r'^$', 'index'),
     (r'^sys/$', 'sys_index'),
+    (r'^sys/add/$', 'sys_config_create'),
+    (r'^sys/(?P<config_id>\d+)/', 'sys_config_edit'),
     (r'^sys/setup/$', 'sys_setup'),
     (r'^opt/$', 'opt_index'),
 )
--- admin/views.py
+++ admin/views.py
@@ -16,6 +16,35 @@ def sys_index(request):
     cfg_list=cfg.getConfigList()
     return render_to_response('admin/sys_index.html', RequestContext(request, { 'config_list': cfg_list }))
 
+def sys_config_create(request):
+    if request.method == 'POST':
+        form = cfg.ConfigForm(request.POST)
+        if form.is_valid():
+            logging.info('form data' + form.cleaned_data['name'])
+            config  = form.save(commit=False)
+            config.save()
+            logging.info('new config created - id: %s key: %s data: %s' % (config.key().id() , config.key(), config))
+            return redirect('/admin/sys/')
+    else:
+        form = cfg.ConfigForm()
+    return render_to_response('admin/sys_config_create.html', RequestContext(request, { 'form': form }))
+
+def sys_config_edit(request, config_id):
+    config = cfg.Config.get_by_id(int(config_id))
+    if config is None:
+        raise Http404
+    if request.method == 'POST':
+        form = cfg.ConfigForm(request.POST, instance=config)
+        if form.is_valid():
+            logging.info('edit config before - id: %s key: %s data: %s' % (config.key().id() , config.key(), config))
+            form.save(commit=False)
+            logging.info('edit config after - id: %s key: %s data: %s' % (config.key().id() , config.key(), config))
+            config.save()
+            return redirect('/admin/sys/')
+    else:
+        form = cfg.ConfigForm(instance=config)
+    return render_to_response('admin/sys_config_edit.html', RequestContext(request, { 'form': form }) )
+
 def sys_setup(request):
     cfg.setupConfig()
     return redirect('/admin/sys/')
