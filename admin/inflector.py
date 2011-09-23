02a56a203cb5257acf9a8e470338bfa72bf84633
Tomas <tomas.hluchan@gmail.com>
2011-09-23 15:50:42 +0200

    sklonovani

--- admin/inflector.py
+++ admin/inflector.py
@@ -6,18 +6,53 @@ import logging
 
 class InflectDict:
     def __init__(self):
-        self.data=dict()
-    
-    def add(self, pattern, value):
-        self.data[pattern]=value 
+        self.root = [None, {}]
 
-    def find(self, pattern):
-        if pattern in self.data:
-            #exact match
-            return self.data[pattern]
-        return None
-#    def __str__(self):
-#        return self.data.__str__()
+    def add(self, pattern, proposal):
+        curr_node = self.root
+        for ch in pattern[::-1]:
+            curr_node = curr_node[1].setdefault(ch, [None, {}])
+        curr_node[0] = (pattern,proposal)
+
+    def find_max(self, key):
+        curr_node = self.root
+        prefix_len = 0
+        for ch in key[::-1]:
+            try:
+                curr_node = curr_node[1][ch]
+                prefix_len+=1
+            except KeyError:
+                if curr_node == self.root:
+                    return (0,None)
+                break
+
+        while curr_node[0] is None:
+            curr_node = curr_node[1].itervalues().next()
+ 
+        return (prefix_len,curr_node[0])
+
+
+
+    def inflect(self, text, default = None):
+        (suffix_len, rule) = self.find_max(text)
+        if suffix_len == 0:
+            return default 
+
+
+        pattern = rule[0]
+        proposal = rule[1]
+
+        if text == pattern:
+            return proposal
+
+        base = text[:-suffix_len]
+        pattern_base = pattern[:-suffix_len]
+        prefix_len = len(pattern_base) 
+        proposal_suffix = proposal[prefix_len:]
+        return base+proposal_suffix
+        
+
+
    
 DICTS=None
 
@@ -30,7 +65,7 @@ def init_dicts():
         dict_key = "%s_%s"%(i.gender,i.part)
         if not dict_key in DICTS:
             DICTS[dict_key]=InflectDict()
-        DICTS[dict_key].add(i.pattern,i)
+        DICTS[dict_key].add(i.pattern,i.proposal)
 
 
 def flush_dicts():
@@ -47,10 +82,10 @@ def do_inflect(part,gender,text):
     logging.info('dict_key=%s, text=%s'%(dict_key,text))
     proposal = None
     if dict_key in DICTS:
-        res = DICTS[dict_key].find(text) 
+        res = DICTS[dict_key].inflect(text) 
         if not res is None:
-            logging.info('use pattern: %s'%res)
-            proposal= res.proposal
+            logging.info('result: %s'%res)
+            proposal = res
         else:
             logging.info('no pattern match!')
     else:
